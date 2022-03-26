074b6d441d4f9e99441f5c6c9d3a875a417c0905
lcobell <lcobell@gmail.com>
2022-03-26 09:57:54 -0500

    Adding home page view

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ require("dotenv").config();
 const express = require("express");
 // initialize the app variable
 const app = express();
+
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 // import the router created in controllers folder
 app.use("/places", require("./controllers/places"));
 // create homepage route
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.render("home");
 });
 // wildcard route, always below all other routes!
 app.get("*", (req, res) => {
