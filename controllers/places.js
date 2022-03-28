e0995cc60c623a5ecfc8ea009fcb49286d183c96
lcobell <lcobell@gmail.com>
2022-03-28 16:57:06 -0500

    I'm getting an error message

--- controllers/places.js
+++ controllers/places.js
@@ -1,30 +1,25 @@
 const router = require("express").Router();
-
+const places = require("../models/places.js");
 // first route, the index page the GET route that will eventually show a list of all places
 // GET /places
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/breakfast-food.jpeg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/peanut-butter-apple.jpeg",
-    },
-  ];
   res.render("places/index", { places });
 });
 
 router.post("/", (req, res) => {
   console.log(req.body);
-  res.send("POST /places");
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 router.get("/new", (req, res) => {
